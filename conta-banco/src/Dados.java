9ead6403d053314a211bb755981e1aba98a17141
daymira <daynaramira@gmail.com>
2023-09-20 14:24:30 -0300

    ContaTerminal e Dados

---
+++ conta-banco/src/Dados.java
@@ -0,0 +1,36 @@
+public class Dados {
+    private String nomeCliente;
+    private String agencia;
+    private int numeroConta;
+    private float saldo;
+
+    public Dados(){
+
+    }
+    
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public float getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+}
